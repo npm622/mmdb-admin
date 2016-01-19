c5b88f8dd493c04694d7ddd7973a0f33e4382f9f
Nick Makes <nick.makes@mlb.com>
2016-01-19 17:03:42 -0500

    Fixes js compilation error

--- src/mmdb-admin.js
+++ src/mmdb-admin.js
@@ -119,12 +119,12 @@
 	function MmdbAdminSearchCtrl() {
 		var vm = this;
 		
-		vm.searchModes = ["id", "data"];
+		vm.searchModes = ['id', 'data'];
 		
 		vm.activeSearchMode = searchModes[0];
 		
 		vm.select = function(idx) {
-			vm selection = vm.searchModes[idx];
+			var selection = vm.searchModes[idx];
 			if (selection == vm.activeSearchMode) {
 				return;
 			}
