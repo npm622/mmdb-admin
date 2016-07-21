237ada12adab88e53e48894256290cbbc9cd10fb
Nick Makes <nick.makes@mlb.com>
2016-07-21 12:28:30 -0400

    Refactors how to configure mmdb-admin page

--- src/app.js
+++ src/app.js
@@ -10,12 +10,27 @@
             vm.json = json;
         };
 
+        vm.state = 'mmdbAdmin';
+        vm.setState = function( state ) {
+            vm.state = state;
+        }
+
+        vm.url = '#/mmdb-admin';
+        vm.setUrl = function( url ) {
+            vm.url = url;
+        }
+
+        vm.display = 'mmdb admin';
+        vm.setDisplay = function( display ) {
+            vm.display = display;
+        }
+
         vm.page = {
-            state : 'mmdbAdmin',
-            url : '#/mmdb-admin',
-            display : 'mmdb admin',
-            template : '<dashboard></dashboard>'
-        };
+            state : vm.state,
+            url : vm.url,
+            template : '<dashboard></dashboard>',
+            display : vm.display
+        }
 
         vm.$get = function() {
             return vm;
