ea832db379b62b7ddc5f64610d6c44ee4504d489
Nick Makes <nick.makes@mlb.com>
2016-08-05 16:49:42 -0400

    Adds support for keeping both known and managed items

--- src/services/table-service.js
+++ src/services/table-service.js
@@ -3,10 +3,25 @@
 
     angular.module( 'mmdb.admin' )
 
-    .factory( 'Table', [ '$http', '$q', 'Schema', Table ] )
+    .factory( 'Table', [ '$http', '$q', 'Schema', 'Item', Table ] )
 
-    function Table( $http, $q, Schema ) {
+    function Table( $http, $q, Schema, Item ) {
         var webEndpoint = Schema.json.webEndpoint;
+        
+        function restPath(pk) {
+            var restPath = '';
+            if (typeof pk === 'object') {
+                for (var property in pk) {
+                    if (pk.hasOwnProperty(property)) {
+                        restPath += '/' + property + '/' + pk[property];
+                    }
+                }
+            } else {
+                restPath = '/' + pk;
+            }
+            
+            return restPath;
+        }
 
         return {
             fetchAll : function( table ) {
@@ -25,12 +40,20 @@
                 console.log( payload );
                 
                 var deferred = $q.defer();
-
-                $http.post( webEndpoint + '/' + table.contextPath, payload ).then( function( response ) {
-                    deferred.resolve( response.data );
-                }, function() {
-                    deferred.reject();
-                } );
+                
+                if (table.isAutoPk) {
+                    $http.post( webEndpoint + '/' + table.contextPath, payload ).then( function( response ) {
+                        deferred.resolve( response.data );
+                    }, function() {
+                        deferred.reject();
+                    } );
+                } else {
+                    $http.put( webEndpoint + '/' + table.contextPath + restPath(Item.determinePk(table, Angular.fromJson(payload))), payload ).then( function( response ) {
+                        deferred.resolve( response.data );
+                    }, function() {
+                        deferred.reject();
+                    } );
+                }
 
                 return deferred.promise;
             },
@@ -38,20 +61,9 @@
                 console.log( 'deleting...' );
                 console.log( pk );
                 
-                var restPath = '';
-                if (typeof pk === 'object') {
-                    for (var property in pk) {
-                        if (pk.hasOwnProperty(property)) {
-                            restPath += '/' + property + '/' + pk[property];
-                        }
-                    }
-                } else {
-                    restPath = '/' + pk;
-                }
+                var deferred = $q.defer();
                 
-                var deferred $q.defer();
-                
-                $http.delete( webEndpoint + '/' + table.contextPath + restPath ).then( function( response ) {
+                $http.delete( webEndpoint + '/' + table.contextPath + restPath(pk) ).then( function( response ) {
                     deferred.resolve( response.data );
                 }, function() {
                     deferred.reject();
