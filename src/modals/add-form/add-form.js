3aef62b5f16207211a47727a4a03fde0698fe3af
Nick Makes <nick.makes@mlb.com>
2016-08-05 19:25:01 -0400

    Removes unused variables in add-form controller

--- src/modals/add-form/add-form.js
+++ src/modals/add-form/add-form.js
@@ -12,8 +12,6 @@
         controller : function() {
             var vm = this;
 
-            vm.dto = {};
-
             vm.$onInit = function() {
                 var modalInstance = vm.parent.modalInstance;
 
@@ -31,39 +29,6 @@
                 vm.cancel = function() {
                     modalInstance.dismiss( 'cancel' );
                 }
-            }
-
-            function convertDto( dto ) {
-                var item = {};
-
-                for ( var prop in dto ) {
-                    if ( dto.hasOwnProperty( prop ) ) {
-                        var column = findColumn( prop );
-                        if ( column.jsonPath.includes( '.' ) ) {
-                            if ( !item[vm.parent.activeTable.pkKey] ) {
-                                item[vm.parent.activeTable.pkKey] = {};
-                            }
-                            item[vm.parent.activeTable.pkKey][prop] = dto[prop];
-                        } else {
-                            item[prop] = dto[prop];
-                        }
-                    }
-                }
-
-                return item;
-            }
-
-            function findColumn( fieldName ) {
-                for ( var i = 0; i < vm.parent.activeTable.columns.length; i++ ) {
-                    var col = vm.parent.activeTable.columns[i];
-                    if ( col.fieldName === fieldName ) {
-                        return col;
-                    }
-                }
-            }
-
-            function writeJson( item ) {
-                return angular.toJson( item );
             }
         }
     } );
